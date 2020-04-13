0c5cbd4eccc11431a23aeae08a709dda771bec73
fulara <ntszar@gmail.com>
2020-04-13 15:20:53 -0700

    Cleanup, and improve output readibility

--- cracker.sh
+++ cracker.sh
@@ -1,43 +1,84 @@
 #!/bin/bash
 
+function error_exit { echo -e "\e[31m${@}" 1>&2; exit 1; }
+function warn { echo -e "\e[33m${@}"; }
+function info { echo -e "\e[32m${@}"; }
+
+function usage
+{
+  cat << EOF
+usage: cracker.sh [-r --enable-rh-check] conan_package binaries...
+EOF
+  if [[ -n "$1" ]]; then
+    warn "whereas you invoked:"
+    warn "cracker.sh ${@}"
+  fi
+}
+
 function ask_user {
   while true; do
     read -p "$1 Do you wish to $2 Yn " yn
     case $yn in
-      [Yy]* ) return 0; break;;
+      [Yy]* ) return 0;;
       [Nn]* ) return 1;;
       * ) return 0;;
   esac
 done
 }
 
-if [ -z ${CRACKER_CACHE_DIR} ]; then
-  CRACKER_CACHE_DIR=~/.cracker_cache
-else
-  CRACKER_CACHE_DIR=${CRACKER_CACHE_DIR}/.cracker_cache
-fi
+function validate_args {
+  if [ "${#args[@]}" -lt 2 ]; then
+    usage ${orig_args[@]}
+    exit 1
+  fi
+}
 
-CRACKER_CACHE_DIR=${CRACKER_CACHE_DIR:-~/.cracker_cache}
+function init_cache {
+  if [ -z ${CRACKER_CACHE_DIR} ]; then
+    CRACKER_CACHE_DIR=~/.cracker_cache
+  else
+    CRACKER_CACHE_DIR=${CRACKER_CACHE_DIR}/.cracker_cache
+  fi
+  
+  CRACKER_CACHE_DIR=${CRACKER_CACHE_DIR:-~/.cracker_cache}
+  
+  mkdir -p $CRACKER_CACHE_DIR
+}
 
-mkdir -p $CRACKER_CACHE_DIR
+#positional args
+orig_args=( "${@}" )
+args=()
 
-if [ "$#" -lt 2 ]; then
-  echo "This script requires at least 2 args: [conan-package] [binaries...]"
-  exit 1
-fi
+# named args
+while [ "$1" != "" ]; do
+    case "$1" in
+        -r | --enable-rh-check )      RH_CHECK=yes;;
+        -s | --some_more_args )       some_more_args="$2";     shift;;
+        -y | --yet_more_args )        yet_more_args="$2";      shift;;
+        -h | --help )                 usage;                   exit;; # quit and show usage
+        * )                           args+=("$1")             # if no match, add it to the positional args
+    esac
+    shift # move to next kv pair
+done
 
-if ! command -v conan > /dev/null ; then
-  echo "conan is not present in your path."
-  exit 1
-fi
+# restore positional args
+set -- "${args[@]}"
 
+validate_args
+init_cache
+
+command -v conan > /dev/null || error_exit "conan is not present in your path"
 conan_package=$1
 
-if [[ ! "$1" = *"@"* ]]; then
+if [[ ! "$conan_package" = *"/"* ]]; then
+  error_exit "Selected package:\"$conan_package\" does not contain slash? required form of: name/version."
+fi 
+
+if [[ ! "$conan_package" = *"@"* ]]; then
   conan_package=${conan_package}@
 fi
 
-conan search $conan_package &> /dev/null  || ( echo "Package $conan_package not found in cache." ; exit 2 )
+conan search $conan_package &> /dev/null  || error_exit "Package $conan_package not found in cache."
 shift
 
 conan_package_name=$(echo "$conan_package" | cut -d'/' -f 1)
@@ -46,20 +87,20 @@ pkg_index=$pkg_dir/.cracker_index
 
 if [[ -d $pkg_dir ]]; then
   if ask_user "Package $conan_package already cracked" "override?"; then
-    echo "deleting $conan_package from cracker_cache and all its installed packages."
+    info "deleting $conan_package from cracker_cache and all its installed packages."
     for p in $(cat $pkg_index 2>/dev/null ); do 
-       echo "deleting: ${CRACKER_CACHE_DIR}/$p"
+       info "deleting: ${CRACKER_CACHE_DIR}/$p"
        rm -rf ${CRACKER_CACHE_DIR}/$p
 
     done
     rm -rf $pkg_dir
   else 
-    echo "not overriding existing package - exiting."
+    warn "not overriding existing package - exiting."
     exit 0
   fi
 fi 
 
-conan install $conan_package -g virtualenv -g virtualrunenv -if $pkg_dir &>/dev/null || ( echo "Failed to install $conan_package_name" ; exit 3 )
+conan install $conan_package -g virtualenv -g virtualrunenv -if $pkg_dir &>/dev/null || error_exit "Failed to install $conan_package_name"
 
 while test ${#} -gt 0
 do
@@ -68,15 +109,15 @@ do
   shift
   if [ -f $location ] ; then
     if ask_user "Location $location is already occupied" "overwrite with $what from $conan_package_name?"; then
-      echo "overriding $what."
-      echo "WARN: this will not remove the mention of that package from its creator index! may result in unexpected issues." 
+      info "overriding $what."
+      warn "this will not remove the mention of that package from its creator index! may result in unexpected issues." 
       rm $location
     else
       continue
     fi
   fi
   if [ -d $location ]; then
-    echo "Location $location is already occupied by a directory - possibly package with $what name exists, skipping"
+    warn "Location $location is already occupied by a directory - possibly package with $what name exists, skipping"
     continue
   fi
   echo "Installing wrapper for: $what in $location"  
