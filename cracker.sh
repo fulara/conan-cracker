3cf952a4eda3b6813c430ea6ea302efb1d1b5fa8
fulara <ntszar@gmail.com>
2020-04-15 16:53:59 -0700

    Deduce, and few more options.

--- cracker.sh
+++ cracker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #todo:
-#1. implement --enable-rh-check
-#2. cleanup usage.
-#3. ?
+#* deduce binaries
+#*. cleanup usage.
+#*. ?
 
 function error_exit { echo -e "\e[31m${@}" 1>&2; exit 1; }
 function warn { echo -e "\e[33m${@}"; }
@@ -12,7 +12,7 @@ function info { echo -e "\e[32m${@}"; }
 function usage
 {
   cat << EOF
-usage: cracker.sh [-r --enable-rh-check] conan_package binaries...
+usage: cracker.sh [-r --disable-rh-check] [-d --deduce ] [-g --global-cache] conan_package [binaries...]
 EOF
   if [[ -n "$1" ]]; then
     warn "whereas you invoked:"
@@ -21,6 +21,9 @@ EOF
 }
 
 function rh6_check {
+  if [ -n $skip_rh_check ]; then
+    return 0
+  fi
   if [ -f /etc/redhat-release ]; then
     major_v=$(sed -r  's@.*release ([0-9])+\..*@\1@g' /etc/redhat-release)
     if [[ "$major_v" != 6 ]]; then
@@ -37,16 +40,21 @@ function ask_user {
     case $yn in
       [Yy]* ) return 0;;
       [Nn]* ) return 1;;
-      * ) return 0;;
+      * ) echo "yn please.";
   esac
 done
 }
 
 function validate_args {
-  if [ "${#args[@]}" -lt 2 ]; then
+  expected_arg_count=2
+  if [[ -n "$deduce_binaries" ]]; then
+    expected_arg_count=1
+  fi
+  if [ "${#args[@]}" -lt $expected_arg_count ]; then
     usage ${orig_args[@]}
     exit 1
   fi
+  rh6_check
 }
 
 function init_cache {
@@ -62,6 +70,34 @@ function init_cache {
   mkdir -p $CRACKER_CACHE_DIR/.cracker_storage
 }
 
+function crack {
+  what=$1
+  location=${CRACKER_CACHE_DIR}/$1
+  shift
+  if [ -f $location ] ; then
+    if ask_user "Location $location is already occupied" "overwrite with $what from $conan_package_name?"; then
+      info "overriding $what."
+      warn "this will not remove the mention of that package from its creator index! may result in unexpected issues."
+      rm $location
+    else
+      continue
+    fi
+  fi
+  if [ -d $location ]; then
+    warn "Location $location is already occupied by a directory - possibly package with $what name exists, skipping"
+    continue
+  fi
+  echo "Installing wrapper for: $what in $location"  
+  cat <<EOT >> $location
+#!/bin/bash
+source $pkg_dir/activate_run.sh
+source $pkg_dir/activate.sh
+$what "\${@}"
+EOT
+  chmod +x $location
+  echo "$what" >> $pkg_index
+}
+
 #positional args
 orig_args=( "${@}" )
 args=()
@@ -69,9 +105,9 @@ args=()
 # named args
 while [ "$1" != "" ]; do
   case "$1" in
-    -r | --enable-rh-check )      rh6_check;;
-    -s | --some_more_args )       some_more_args="$2";     shift;;
-    -y | --yet_more_args )        yet_more_args="$2";      shift;;
+    -r | --disable-rh-check )     skip_rh_check=yes;;
+    -d | --deduce ) deduce_binaries=yes;;
+    -g | --global-storage ) global_storage=yes;;
     -h | --help )                 usage;                   exit;; # quit and show usage
     * )                           args+=("$1")             # if no match, add it to the positional args
     esac
@@ -123,7 +159,10 @@ if [[ -d $pkg_dir ]]; then
 fi 
 
 previous_storage_path=$(conan config get storage.path)
-conan config set storage.path=${CRACKER_CACHE_DIR}/.cracker_storage
+#if global storage is set - we dont override storage.path.
+if [[ -z $global_storage ]]; then
+  conan config set storage.path=${CRACKER_CACHE_DIR}/.cracker_storage
+fi
 conan install ${conan_package} -g virtualenv -g virtualrunenv -if $pkg_dir 2>&1 || {
   conan config set storage.path=${previous_storage_path}
   echo $output
@@ -131,31 +170,16 @@ conan install ${conan_package} -g virtualenv -g virtualrunenv -if $pkg_dir 2>&1 
 }
 conan config set storage.path=${previous_storage_path} || error_exit "Unable to revert your storage path to previous value, your conan installation is now corrupted, previous path was: $previous_storage_path"
 
-while test ${#} -gt 0
-do
-  what=$1
-  location=${CRACKER_CACHE_DIR}/$1
-  shift
-  if [ -f $location ] ; then
-    if ask_user "Location $location is already occupied" "overwrite with $what from $conan_package_name?"; then
-      info "overriding $what."
-      warn "this will not remove the mention of that package from its creator index! may result in unexpected issues."
-      rm $location
-    else
-      continue
-    fi
+if [[ -n $deduce_binaries ]]; then
+  pkg_path=$(sed -n -r 's@^PATH="([^"]+).*@\1@p' $pkg_dir/environment_run.sh.env)
+  if [[ -z $pkg_path ]] || [[ ! -d $pkg_path ]]; then
+    error_exit "You wanted binary deduction but your binary does not seem to have valid PATH?"
   fi
-  if [ -d $location ]; then
-    warn "Location $location is already occupied by a directory - possibly package with $what name exists, skipping"
-    continue
-  fi
-  echo "Installing wrapper for: $what in $location"  
-  cat <<EOT >> $location
-#!/bin/bash
-source $pkg_dir/activate_run.sh
-source $pkg_dir/activate.sh
-$what "\${@}"
-EOT
-  chmod +x $location
-  echo "$what" >> $pkg_index
+  for bin in $(find $pkg_path -maxdepth 1 -type f -executable -printf "%f\n" ); do
+    crack $bin
+  done
+fi
+
+while test ${#} -gt 0; do
+  crack $1
 done
