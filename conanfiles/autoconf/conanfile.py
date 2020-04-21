dc8cf0462f99efc3ef7afee1188aa74235945b0a
fulara <ntszar@gmail.com>
2020-04-21 02:11:30 +0200

    Update conanfile.py

--- conanfiles/autoconf/conanfile.py
+++ conanfiles/autoconf/conanfile.py
@@ -40,13 +40,19 @@ class GitConan(ConanFile):
             with tools.environment_append(be.vars):
                 with tools.environment_append(re.vars):
                     be.install()
-                                        
+                                                              
     def package_info(self):
         self.env_info.AUTOCONF = os.path.join(self.package_folder, "bin", "autoconf")
         self.env_info.AUTOM4TE = os.path.join(self.package_folder, "bin", "autom4te")
+        autoconf_dir = os.path.join(self.package_folder, "share", "autoconf")
         self.env_info.AC_MACRODIR = os.path.join(self.package_folder, "share", "autoconf")
+        cfg = os.path.join(self.package_folder, "share", "autoconf", "autom4te.cfg")
         self.env_info.AUTOM4TE_CFG = os.path.join(self.package_folder, "share", "autoconf", "autom4te.cfg")
         
         self.env_info.autom4te_perllibdir.append(os.path.join(self.package_folder, "share", "autoconf"))
         self.env_info.PERL5LIB.append(os.path.join(self.package_folder, "share", "autoconf"))
+
+        #uber hacky but maybe works.
+        # unfortunatelly autom4te.cfg has hardcoded strings.. so we just rewrite them everytime!
+        self.run("sed -i -r \"s@prepend-include .*@prepend-include '{}'@g\" {}".format(autoconf_dir, cfg))
         
